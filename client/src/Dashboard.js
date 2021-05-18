e184c2e3c5127d5c3ab85f0456d3e668645933d4
Matt Salmons <mattcsalmons@gmail.com>
2021-05-18 10:30:33 -0600

    created cancel flag to hold back search requests until finished typing

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -11,26 +11,41 @@ export default function Dashboard({ code }) {
   const accessToken = useAuth(code);
   const [search, setSearch] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  console.log(searchResults)
 
   useEffect(() => {
-    if (!accessToken) {
-      return;
-    }
-     spotifyApi.setAccessToken(accessToken)
+    if (!accessToken) return;
+    spotifyApi.setAccessToken(accessToken)
   }, [accessToken])
 
   useEffect(() => {
-    if (!search) {
-      return setSearchResults([]);
-    }
-    if (!accessToken) {
-      return;
-    }
+    if (!search) return setSearchResults([])
+    if (!accessToken) return
 
-    spotifyApi.searchTracks(search)
-      .then(res => {
-        console.log(res.body.tracks.items);
-      })
+    let cancel = false
+    spotifyApi.searchTracks(search).then(res => {
+      if (cancel) return
+      setSearchResults(
+        res.body.tracks.items.map(track => {
+          const smallestAlbumImage = track.album.images.reduce(
+            (smallest, image) => {
+              if (image.height < smallest.height) return image
+              return smallest
+            },
+            track.album.images[0]
+          )
+
+          return {
+            artist: track.artists[0].name,
+            title: track.name,
+            uri: track.uri,
+            albumUrl: smallestAlbumImage.url,
+          }
+        })
+      )
+    })
+
+    return () => (cancel = true)
   }, [search, accessToken])
 
   const onChange = (e) => {
