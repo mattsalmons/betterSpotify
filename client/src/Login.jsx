6ff90b5b176dbf547e8fa4b1a46043c3b1a0eca8
Matt Salmons <mattcsalmons@gmail.com>
2021-05-17 21:22:01 -0600

    created Login component

---
+++ client/src/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Container } from 'react-bootstrap';
+
+const AUTH_URL = 'https://accounts/spotify.com/authorize?client_id=ad631922d2b44460a0572ea8326d9e7a&response_type=code&redirect_uri=http://localhost:3000&scope=%20streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state'
+
+export default function Login() {
+  return (
+    <Container>
+      <a className="btn btn-success btn-lg" href={AUTH_URL}>
+        Login With Spotify
+      </a>
+    </Container>
+  )
+}
