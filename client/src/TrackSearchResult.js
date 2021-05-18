da18e6240b1234f53e81cbd770b926e6f4dbc135
Matt Salmons <mattcsalmons@gmail.com>
2021-05-18 10:48:37 -0600

    renders track name and artist

--- client/src/TrackSearchResult.js
+++ client/src/TrackSearchResult.js
@@ -1,13 +1,23 @@
 import React from 'react'
 
-export default function TrackSearchResult({ track }) {
+export default function TrackSearchResult({ track, chooseTrack }) {
+  const handlePlay = () => {
+    chooseTrack(track);
+  }
   return (
-    <div className='d-flex m-2 align-itesm-center'>
+    <div
+      className="d-flex m-2 align-items-center"
+      style={{ cursor: 'pointer' }}
+      onClick={handlePlay}>
       <img
         src={track.albumUrl}
         style={{ height: '75px', width: '75px' }}
         alt=''
       />
+      <div style={{ marginLeft: '10px'}}>
+        <div>{track.title}</div>
+        <div className="text-muted">{track.artist}</div>
+      </div>
     </div>
   )
 }
